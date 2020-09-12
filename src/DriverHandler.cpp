f162ced2e2cfc548ce961360f7ad73f414495b74
Kejoko <keeganjkochis@gmail.com>
2020-09-12 12:41:26 -0600

    Update matrix with clear, move, scale. Rotation is WIP

--- src/DriverHandler.cpp
+++ src/DriverHandler.cpp
@@ -2,8 +2,11 @@
 // 831455801
 // kkochis@rams.colostate.edu
 
+#include <math.h>
+
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 #include <Eigen/Dense>
@@ -20,8 +23,6 @@ DriverHandler::DriverHandler(const std::string& fileName) {
 }
 
 void DriverHandler::read_driver() {
-    std::cout << mTransformationMatrix << '\n';
-    
     std::ifstream driverStream(mFileName);
     std::string line;
     
@@ -39,6 +40,70 @@ void DriverHandler::read_driver() {
 
 void DriverHandler::update_matrix(const std::string& line) {
     std::cout << line << '\n';
+    
+    if (line == "clear") {
+        mTransformationMatrix <<    1, 0, 0, 0,
+                                    0, 1, 0, 0,
+                                    0, 0, 1, 0,
+                                    0, 0, 0, 1;
+    }
+    else {
+        std::istringstream infoStream(line);
+        std::string type;
+        double x, y, z;
+        Eigen::Matrix4d modifier;
+        
+        infoStream >> type >> x >> y >> z;
+        
+        if (type == "move") {
+            modifier << 1, 0, 0, x,
+                        0, 1, 0, y,
+                        0, 0, 1, z,
+                        0, 0, 0, 1;
+        }
+        else if (type == "rota") {
+            double theta;
+            infoStream >> theta;
+            
+            //Calculate Rz, the rotation matrix about the z axis
+            
+            Eigen::Vector3d W(x, y, z);
+            W = W / sqrt(x*x + y*y + z*z);
+            
+            Eigen::Vector3d M = W;
+            if (M(0) < M(1) && M(0) < M(2))
+                M(0) = 1;
+            else if (M(1) < M(0) && M(1) < M(2))
+                M(1) = 1;
+            else if (M(2) < M(0) && M(2) < M(1))
+                M(2) = 1;
+            M = M / sqrt( M(0)*M(0) + M(1)*M(1) + M(2)*M(2) );
+            
+            Eigen::Vector3d U = W.cross(M);
+            
+            Eigen::Vector3d V = W.cross(U);
+            
+            Eigen::Matrix4d Rw;
+            Rw <<   U(0), U(1), U(2), 0,
+                    V(0), V(1), V(2), 0,
+                    W(0), W(1), W(2), 0,
+                       0,    0,    0, 1;
+            
+            //Calculate Rt
+            
+            //modifer = Rt * Rz * Rw
+        }
+        else if (type == "scale") {
+            modifier << x, 0, 0, 0,
+                        0, y, 0, 0,
+                        0, 0, z, 0,
+                        0, 0, 0, 1;
+        }
+        
+        mTransformationMatrix = mTransformationMatrix * modifier;
+    }
+    
+    std::cout << mTransformationMatrix << '\n';
 }
 
 void DriverHandler::load_object(const std::string& line) {
